6435363548df86609e45218eb3aa336a2a65c39b
Emre Özdil <emreozdil@icloud.com>
2018-05-20 13:58:51 +0300

    Init Term Project

---
+++ trie.hs
@@ -0,0 +1,26 @@
+import Data.Map
+import Data.Maybe
+import System.Environment
+import System.IO
+import Prelude hiding (Word)
+
+data Trie = Trie {end :: Bool, children :: Map Char Trie}
+type Word = String
+
+empty :: Trie
+empty = undefined
+
+insert :: Word -> Trie -> Trie
+insert = undefined
+
+insertList :: [Word] -> Trie
+insertList = undefined
+
+search :: Word -> Trie -> Bool
+search = undefined
+
+getWords :: Trie -> [Word]
+getWords = undefined
+
+prefix :: Word -> Trie -> Maybe [Word]
+prefix = undefined
